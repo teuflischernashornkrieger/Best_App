f27f1d6014f079bf285c435488739cf12bd1cb8c
teuflischernashornkrieger <david.pacher@gmx.net>
2019-10-07 15:06:02 +0200

    added first comment

--- app/src/main/java/com/example/bestapp/MainActivity.java
+++ app/src/main/java/com/example/bestapp/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Meine erste App - Das ist die Java-Datei zur Hauptoberfläche
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
+
